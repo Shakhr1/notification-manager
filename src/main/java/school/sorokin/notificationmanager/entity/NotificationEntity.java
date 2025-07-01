13c58cae8b1ff59ce38cc6f5f00004bde446e050
Shakh Shukhratov <shakhriplay@gmail.com>
2025-07-01 17:15:37 +0300

    hot fix and refactoring

--- src/main/java/school/sorokin/notificationmanager/entity/NotificationEntity.java
+++ src/main/java/school/sorokin/notificationmanager/entity/NotificationEntity.java
@@ -2,13 +2,18 @@ package school.sorokin.notificationmanager.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "notifications")
-@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
 public class NotificationEntity {
 
     @Id
@@ -39,4 +44,20 @@ public class NotificationEntity {
     private String newEventStatus;
 
     private Boolean isRead;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        NotificationEntity that = (NotificationEntity) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
 }
